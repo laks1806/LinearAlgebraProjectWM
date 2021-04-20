f38051c82d2d36bc82748a106c5c38c2b45ad5d9
laks1806 <67590424+laks1806@users.noreply.github.com>
2021-04-20 22:37:18 +0530

    Add files via upload

---
+++ try2.py
@@ -0,0 +1,27 @@
+#Import Linear Regression model from scikit-learn.
+from sklearn.linear_model import LinearRegression
+def linear_regression(data, power, models_to_plot):
+    #initialize predictors:
+    predictors=['x']
+    if power>=2:
+        predictors.extend(['x_%d'%i for i in range(2,power+1)])
+    
+    #Fit the model
+    linreg = LinearRegression(normalize=True)
+    linreg.fit(data[predictors],data['y'])
+    y_pred = linreg.predict(data[predictors])
+    
+    #Check if a plot is to be made for the entered power
+    if power in models_to_plot:
+        plt.subplot(models_to_plot[power])
+        plt.tight_layout()
+        plt.plot(data['x'],y_pred)
+        plt.plot(data['x'],data['y'],'.')
+        plt.title('Plot for power: %d'%power)
+    
+    #Return the result in pre-defined format
+    rss = sum((y_pred-data['y'])**2)
+    ret = [rss]
+    ret.extend([linreg.intercept_])
+    ret.extend(linreg.coef_)
+    return ret
